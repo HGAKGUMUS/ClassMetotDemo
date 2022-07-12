1d15d84e4c8aa5f94028cad15506e749f2e41f1f
DTHAKGUMUS <DTHAKGUMUS@DTHAKGUMUSNB>
2022-07-12 15:06:07 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    internal class MusteriManager
+    {
+
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Müsteri Eklendi : " + musteri.Id + " " + musteri.Ad + " " + musteri.Soyad + " " + musteri.Maas);
+
+        }
+
+        public void Listele(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Listele : " + musteri.Ad + " " + musteri.Soyad);
+        }
+
+        public void Sil(Musteri musteri)
+        {
+            Console.Clear();
+            Console.WriteLine("Belirtilen bilgilerdeki müşteri silindi!. - " + musteri.Id + " "  + musteri.Ad + " " + musteri.Soyad + " " + musteri.Maas );
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ClassMetotDemo;
+
+
+
+
+  Musteri musteri1=new Musteri();
+  musteri1.Id= 001;
+  musteri1.Ad="Ali";
+  musteri1.Soyad="Sarilar";
+  musteri1.Maas=2500;
+
+  Musteri musteri2=new Musteri();
+  musteri2.Id = 002;
+  musteri2.Ad= "Gökhan";
+  musteri2.Soyad= "Akg";
+  musteri2.Maas=3500;
+
+  Musteri musteri3=new Musteri();;
+  musteri3.Id=003;
+  musteri3.Ad="Büşra";
+  musteri3.Soyad="Gms";
+  musteri3.Maas=4500;
+
+  MusteriManager musteriManager=new MusteriManager();
+  musteriManager.Ekle(musteri1);
+  musteriManager.Ekle(musteri2); 
+  musteriManager.Ekle(musteri3);
+
+  musteriManager.Listele(musteri1);
+  musteriManager.Listele(musteri2);
+  musteriManager.Listele(musteri3);
+
+
+  Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3 };
+
+  foreach (Musteri mliste in musteriler)
+  {
+      Console.WriteLine("Müşteri Id : " + mliste.Id);
+      Console.WriteLine("Müşteri Adı: " + mliste.Ad);
+      Console.WriteLine("Müşteri Soyadı: " + mliste.Soyad);
+      Console.WriteLine("Müşteri Maas: " + mliste.Maas);
+  }
+
+
+  
